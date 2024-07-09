7de51a3f56e724d331832f88ca3533aacde9a49b
pkc035 <pkc0305@gmail.com>
2024-07-10 05:40:58 +0900

    fix : docker-compose conf 수정

--- b2tech_test/b2tech_test/settings.py
+++ b2tech_test/b2tech_test/settings.py
@@ -1,8 +1,10 @@
+import os
 from .my_settings import SECRET_KEY, DATABASES
 from datetime import timedelta
 
 SECRET_KEY = SECRET_KEY
 DATABASES = DATABASES
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -109,7 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
